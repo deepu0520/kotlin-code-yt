c85d4e03a01a300627c9a4e31b6182d6d13395ce
SparxitSolution09 <60690248+SparxitSolution09@users.noreply.github.com>
2023-10-28 10:12:59 +0530

    reverse string

--- app/src/main/java/com/example/mycoding/CodingDemo.kt
+++ app/src/main/java/com/example/mycoding/CodingDemo.kt
@@ -2,10 +2,21 @@ class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Hello!")
+            val originalString = "Hello Kotlin"
+            val reverseString = reverseString(originalString)
+            println("origional String: $originalString")
+            println("reverse String: $reverseString")
+        }
+        // reverse string
+
+        fun reverseString(input: String):String{
+            var reverse = ""
+            for(i in input.length -1 downTo 0)
+            // for(int i = input.length-1;i>=o;i--) in java
+                reverse += reverse[i]
+            return reverse
         }
     }
-
 
 
 
